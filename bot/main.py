80938d6c6925a8461f5cd95f663d373fe4d208b1
AnneBoshove <92106585+AnneBoshove@users.noreply.github.com>
2022-03-10 08:59:28 +0100

    added urk, friet. patat

--- bot/main.py
+++ bot/main.py
@@ -82,7 +82,26 @@ async def meme(ctx):
 async def meme(ctx):
     response = "I’ve been thinking. When life gives you lemons? Don’t make lemonade. Make life take the lemons back! Get mad! I don’t want your damn lemons! What am I supposed to do with these? Demand to see life’s manager! Make life rue the day it thought is could give me lemons! Do you know who I am? I’m the man who’s going to burn your house down! With the lemons! I’m going to get my engineers to invent a combustible lemon that burns your house down!"
     await ctx.send(response)
+    
+ 
+@bot.command(name='urk')
+async def meme(ctx):
+    response = 'UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUGGGGGGGGGGGGGGGGGGGHHHHHHHHHHHHHHHHH'
+    await ctx.send(response)   
 
+@bot.command(name='patat')
+async def meme(ctx):
+    response = 'HET IS FRIET!!!'
+    await ctx.send(response)
+    
+    
+@bot.command(name='friet')
+async def meme(ctx):
+    response = 'HET IS PATAT!!!'
+    await ctx.send(response)
+
+    
+    
 ##GIFS
 
 @bot.command(name='chips')
